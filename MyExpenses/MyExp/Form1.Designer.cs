9e90fc68af59e311546d32819f053b89846d03fe
nekita69 <kne.21@uni-dubna.ru>
2023-09-19 23:57:01 +0300

    Update project. Fix drow graphics

---
+++ MyExpenses/MyExp/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace MyExp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(351, 570);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Доход/Расход";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Баланс";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(112, 51);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(450, 300);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(329, 609);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 29);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(237, 684);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 52);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Расход";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(352, 684);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 52);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Доход";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(467, 684);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 52);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Графики";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+            legend1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            legend1.IsTextAutoFit = false;
+            legend1.Name = "Legend1";
+            legend1.ShadowColor = System.Drawing.Color.White;
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 42);
+            this.chart1.Name = "chart1";
+            series1.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Center;
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.LegendText = "Расходы";
+            series1.Name = "Series1";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            series2.Legend = "Legend1";
+            series2.LegendText = "Доходы";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(760, 348);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            this.chart1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 861);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(800, 900);
+            this.MinimumSize = new System.Drawing.Size(800, 900);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyExp";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ MyExpenses/MyExp/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml.Linq;
+
+namespace MyExp
+{
+    public partial class Form1 : Form
+    {
+        App app; // Объект приложения;
+        Dictionary<int, string> mnt = new Dictionary<int, string>(){
+            { 1, "January" },
+            { 2, "February" },
+            { 3, "March" },
+            { 4, "April" },
+            { 5, "May" },
+            { 6, "June" },
+            { 7, "July" },
+            { 8, "August" },
+            { 9, "September" },
+            { 10, "October" },
+            { 11, "November" },
+            { 12, "December" }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            app = new App();
+            app.SplitForMonth();
+
+
+            //chart1.ChartAreas[0].AxisX.LabelStyle.Format = "MMMM";
+
+            //chart1.Series[0].XValueType = ChartValueType.DateTime;
+
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = 12;
+
+            //chart1.ChartAreas[0].AxisY.Minimum = -10;
+            //chart1.ChartAreas[0].AxisY.Maximum = 10;
+
+            chart1.Series[0].Points.Clear();
+
+            
+            for(int i = 0; i < app.monthsXp.Count(); i++)
+            {
+                // Расходы;
+                DataPoint p1 = new DataPoint();
+                p1.AxisLabel = mnt[app.monthsMon[i]];
+
+                p1.XValue = i;
+                p1.YValues = new double[] { Convert.ToDouble(Math.Abs(app.monthsXp[i])) };
+
+                chart1.Series[0].Points.Add(p1);
+
+                // Доходы;
+                p1 = new DataPoint();
+                p1.AxisLabel = mnt[app.monthsMon[i]];
+
+                p1.XValue = i; 
+                p1.YValues = new double[] { Convert.ToDouble(Math.Abs(app.monthsInc[i])) };
+
+                chart1.Series[1].Points.Add(p1);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Расход
+        {
+            string s = textBox2.Text.Trim(' '); //Получаем данные из поля суммы;
+            if (int.TryParse(s, out _))
+            {
+                textBox1.Text = ""; //Очищаем текстбокс для вывода;
+                textBox2.Text = ""; //Очищаем поле вводы суммы;
+                decimal d = Convert.ToDecimal(s);
+                if (d > 0)
+                    d *= -1;
+
+                if (!app.AddHis(d))
+                    MessageBox.Show("Недостаточно средств");
+
+                app.GetHistory(textBox1); //Выводим новую историю та textbox;
+                app.AppSave();
+                label2.Text = "Баланс: " + app.CheckBalance() + " RUB\r\n";
+
+            }
+            else MessageBox.Show("Не является числом");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime localDate = DateTime.Now;
+            string[] datetime = Convert.ToString(localDate).Split(' ');
+
+            textBox1.Text += "Последнее обновление: " + app.lastDate + "\r\n\r\n";
+            app.GetHistory(textBox1);
+            //app.GetMonths(textBox1);
+
+            label2.Text = "Баланс: " + app.CheckBalance() + " RUB\r\n";
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Доход
+        {
+            string s = textBox2.Text.Trim(' '); //Получаем данные из поля суммы;
+            if (int.TryParse(s, out _))
+            {
+                textBox1.Text = ""; //Очищаем текстбокс для вывода;
+                textBox2.Text = ""; //Очищаем поле вводы суммы;
+                decimal d = Convert.ToDecimal(s);
+                if (d < 0)
+                    d *= -1;
+
+                app.AddHis(d);
+                app.GetHistory(textBox1); //Выводим новую историю та textbox;
+                app.AppSave();
+
+                label2.Text = "Баланс: " + app.CheckBalance() + " RUB\r\n";
+            }
+            else MessageBox.Show("Не является числом");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            chart1.Visible = !chart1.Visible;
+
+            if (chart1.Visible)
+            { 
+                button3.Text = "История";
+            }
+            else
+            {
+                button3.Text = "Графики";
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            app.AppSave();
+            Application.Exit();
+        }
+    }
+}
