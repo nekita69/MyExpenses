13c0e09be9454d5060fa2a6434bb5c1162c63698
nekita69 <kne.21@uni-dubna.ru>
2023-09-23 18:12:59 +0300

    New project

---
+++ MyExps/MyExps/FileStorage.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyExps
+{
+    internal class FileStorage
+    {
+        UsersApp usersApp; //Все пользователи приложения;
+
+        public FileStorage() //При инициализации считываем всех пользователей файла;
+        {
+            if (!File.Exists("users.json")) //Файл не существует;
+            {
+                string js = "";
+                File.WriteAllText("users.json", js); //Создаем пустой файл;
+                usersApp = new UsersApp();
+                return;
+            }
+            //Файл существует;
+            string jsread = File.ReadAllText("users.json");
+            if (jsread == "")
+            {
+                usersApp = new UsersApp();
+                return;
+            }
+            usersApp = JsonConvert.DeserializeObject<UsersApp>(jsread);
+        }
+
+        public User FindFile(string log, string pass) //Поиск пользователя;
+        {
+            //Если нашел - Объект пользователя, не нашел - null;
+            return usersApp.FindUser(log, pass);
+        }
+
+        public bool AddFile(User user) //Добавление пользователя;
+        {
+            //Не добавит с равными logins;
+            for (int i = 0; i < usersApp.users.Count; i++)
+                if (usersApp.users[i].login.ToUpper() == user.login.ToUpper())
+                    return false;
+            usersApp.users.Add(user);
+            return true;
+        }
+
+        public bool ChangeFile(User user) //Изменение объекта в хранилище;
+        {
+            for (int i = 0; i < usersApp.users.Count; i++)
+                if (usersApp.users[i].login.ToUpper() == user.login.ToUpper())
+                {
+                    usersApp.users[i] = user;
+                    return true;
+                }
+            return false;
+        }
+
+        public void SaveFile() //Сохранение файла;
+        {
+            string js = JsonConvert.SerializeObject(usersApp);
+            File.WriteAllText("users.json", js);
+            usersApp = null;
+        }
+    }
+}
---
+++ MyExps/MyExps/User.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExps
+{
+    internal class User //Класс пользователя;
+    {
+        public string login = "";
+        public string password = "";
+        public List<string> history { get; set; } //История за месяц;
+        public List<string> monhist { get; set; } //История за год;
+
+        public User() { } //Конструктор по умолчанию, нужен для десерилизации;
+
+        public User(string log, string psss)
+        {
+            login = log;
+            password = psss;
+
+            history = new List<string>();
+            monhist = new List<string>();
+        }
+    }
+
+
+    internal class UsersApp //Класс для серилизации всех пользователей приложения;
+    {
+        public List<User> users = new List<User>();
+        public UsersApp() { }
+
+        public User FindUser(string log, string pass) //Поиск пользователя;
+        {
+            for(int i = 0; i < users.Count; i++)
+                if (users[i].login == log && users[i].password == pass)
+                    return users[i];
+            return null;
+        }
+    }
+}
