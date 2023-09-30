4b0720d322bea8291838eff3837f0a758a60a3c1
nekita69 <kne.21@uni-dubna.ru>
2023-09-30 15:37:29 +0300

    Изменение

---
+++ MyExps/MyExps/Storage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExps
+{
+    internal class Storage
+    {
+        FileStorage file;
+
+        public Storage()
+        {
+            file = new FileStorage();
+        }
+
+        public User Find(string login,  string password) //Поиск в хранилище;
+        {
+            return file.FindFile(login, password);
+        }
+
+
+
+        // Да
+    }
+}
